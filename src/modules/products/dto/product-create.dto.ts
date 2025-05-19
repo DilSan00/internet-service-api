356c31085c0547314f14f611ddf428a4f1c19ce5
DilSan00 <norms777666@gmail.com>
2025-05-19 14:44:40 +0600

    fix/test with file types

--- src/modules/products/dto/product-create.dto.ts
+++ src/modules/products/dto/product-create.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsNotEmpty, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 export class ProductCreateDto {
-  @ApiProperty({ type: 'string', format: 'binary', required: false })
-  @IsOptional()
-  image: any;
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Поле не должно быть пустым' })
+  // @IsString({ message: 'Поле должно быть строкой' })
+  image: File;
 
   @ApiProperty()
   @IsNotEmpty({ message: 'Поле  не должно быть пустым' })
@@ -21,13 +22,13 @@ export class ProductCreateDto {
   @IsNotEmpty({ message: 'Поле не должно быть пустым' })
   @Type(() => Number)
   @IsNumber({}, { message: 'Поле должно быть числом' })
-  // @Min(0, { message: 'Цена не может быть меньше 0' })
+  @Min(0, { message: 'Цена не может быть меньше 0' })
   price: number;
 
   @ApiProperty()
   @IsNotEmpty({ message: 'Поле не должно быть пустым' })
   @Type(() => Number)
   @IsNumber({}, { message: 'Поле должно быть числом' })
-  // @Min(0, { message: 'Количество не может быть меньше 0' })
+  @Min(0, { message: 'Количество не может быть меньше 0' })
   quantity: number;
 }
